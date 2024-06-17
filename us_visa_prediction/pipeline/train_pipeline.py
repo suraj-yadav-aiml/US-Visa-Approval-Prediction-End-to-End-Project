219b2226a83163e8334c995724060f0949ffd997
Suraj Yadav <qwertypad7612@gmail.com>
2024-06-17 16:55:31 +0530

    Model Traning done

--- us_visa_prediction/pipeline/train_pipeline.py
+++ us_visa_prediction/pipeline/train_pipeline.py
@@ -5,14 +5,17 @@ from us_visa_prediction.logger import logging
 from us_visa_prediction.components.data_ingestion import DataIngestion
 from us_visa_prediction.components.data_validation import DataValidation
 from us_visa_prediction.components.data_transformation import DataTransformation
+from us_visa_prediction.components.model_trainer import ModelTrainer
 
 from us_visa_prediction.entity.config_entity import (DataIngestionConfig,
                                                      DataValidationConfig,
-                                                     DataTransformationConfig)
+                                                     DataTransformationConfig,
+                                                     ModelTrainerConfig)
 
 from us_visa_prediction.entity.artifact_entity import (DataIngestionArtifact,
                                                        DataValidationArtifact,
-                                                       DataTransformationArtifact)
+                                                       DataTransformationArtifact,
+                                                       ModelTrainerArtifact)
 
 
 
@@ -21,6 +24,7 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
     
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -75,6 +79,20 @@ class TrainPipeline:
             return data_transformation_artifact
         except Exception as e:
             raise USvisaException(e, sys)
+    
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model training
+        """
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise USvisaException(e, sys)
         
     
     def run_pipeline(self, ) -> None:
@@ -86,6 +104,7 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(
                 data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
 
         
         except Exception as e:
