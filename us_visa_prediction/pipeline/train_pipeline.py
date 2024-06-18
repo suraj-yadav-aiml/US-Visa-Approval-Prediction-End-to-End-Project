c29c46f9ee3e05291c0ec6f778b9fbbcf070099b
Suraj Yadav <qwertypad7612@gmail.com>
2024-06-18 13:08:46 +0530

    Model Pusher Done

--- us_visa_prediction/pipeline/train_pipeline.py
+++ us_visa_prediction/pipeline/train_pipeline.py
@@ -7,18 +7,21 @@ from us_visa_prediction.components.data_validation import DataValidation
 from us_visa_prediction.components.data_transformation import DataTransformation
 from us_visa_prediction.components.model_trainer import ModelTrainer
 from us_visa_prediction.components.model_evaluation import ModelEvaluation
+from us_visa_prediction.components.model_pusher import ModelPusher
 
 from us_visa_prediction.entity.config_entity import (DataIngestionConfig,
                                                      DataValidationConfig,
                                                      DataTransformationConfig,
                                                      ModelTrainerConfig,
-                                                     ModelEvaluationConfig)
+                                                     ModelEvaluationConfig,
+                                                     ModelPusherConfig)
 
 from us_visa_prediction.entity.artifact_entity import (DataIngestionArtifact,
                                                        DataValidationArtifact,
                                                        DataTransformationArtifact,
                                                        ModelTrainerArtifact,
-                                                       ModelEvaluationArtifact)
+                                                       ModelEvaluationArtifact,
+                                                       ModelPusherArtifact)
 
 
 
@@ -29,6 +32,7 @@ class TrainPipeline:
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
         self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
     
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -113,22 +117,42 @@ class TrainPipeline:
             raise USvisaException(e, sys)
         
     
+    def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model pushing
+        """
+        try:
+            model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
+                                       model_pusher_config=self.model_pusher_config
+                                       )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
+        
+    
     def run_pipeline(self, ) -> None:
         """
         This method of TrainPipeline class is responsible for running complete pipeline
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-
+            print("Data Ingestion Done")
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-
+            print("Data Validation Done")
             data_transformation_artifact = self.start_data_transformation(
                 data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
-            
+            print("Data Transformation Done")
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
-            
+            print("Model Traning Done")
             model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
                                                                     model_trainer_artifact=model_trainer_artifact)
+            print("Model Evaluation Done")
+            if not model_evaluation_artifact.is_model_accepted:
+                logging.info(f"Model not accepted.")
+                return None
+            
+            model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
 
         
         except Exception as e:
